8bb2617af2c48f9d98e0bf4f2f7cde9b54afbac7
You1994sef <alhamdani.yousef@gmail.com>
2018-10-15 17:56:02 +0800

    Setup the store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Router from './Router';
+import Navigation from './Navigation';
 
 
 class App extends Component {
   render() {
-    return <div className="App">
-        <h1>Minion Shop</h1>
+    return <div className="page-container">
+    <Navigation />
         <Router />
       </div>
   }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './config/store';
 
-const app = <BrowserRouter>
+const app = <Provider store={store}><BrowserRouter>
 <App />
 </BrowserRouter>
+</Provider>
 ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
